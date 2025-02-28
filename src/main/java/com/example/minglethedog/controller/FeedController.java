45a23f705b4dfd2dd1a8187979d7c361e703b114
hyeyoung-hailey <hyeyounghaileysim@gmail.com>
2025-02-28 21:15:35 +0900

    feat: 피드 조회 API 엔드포인트 추가 - lastCursor와 Pageable을 이용한 커서 기반 페이징 지원

--- src/main/java/com/example/minglethedog/controller/FeedController.java
+++ src/main/java/com/example/minglethedog/controller/FeedController.java
@@ -1,2 +1,33 @@
-package com.example.minglethedog.controller;public class FeedController {
+package com.example.minglethedog.controller;
+
+import com.example.minglethedog.common.ApiResponse;
+import com.example.minglethedog.common.ListEntityBody;
+import com.example.minglethedog.dto.CustomUserDetails;
+import com.example.minglethedog.dto.PostResponse;
+import com.example.minglethedog.service.FeedService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/feed")
+@RequiredArgsConstructor
+public class FeedController {
+    private final FeedService feedService;
+
+    @GetMapping
+    public ResponseEntity<ApiResponse<ListEntityBody<PostResponse>>> getFeed(
+            @AuthenticationPrincipal CustomUserDetails user
+            , @RequestParam(required = false) Long lastCursor, Pageable pageable) {
+
+        ApiResponse<ListEntityBody<PostResponse>> response = feedService.getFeed(user.getUserId(), lastCursor, pageable);
+        return ResponseEntity.ok(response);
+    }
 }
