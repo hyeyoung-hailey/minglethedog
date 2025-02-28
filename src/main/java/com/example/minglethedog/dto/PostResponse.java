ce2a64acc5cab1ce19595d78e765f606ba869cdd
hyeyoung-hailey <hyeyounghaileysim@gmail.com>
2025-02-28 21:09:02 +0900

    feat: Add JsonInclude for non null fields in PostResponse, add PostResponse.of() static method

--- src/main/java/com/example/minglethedog/dto/PostResponse.java
+++ src/main/java/com/example/minglethedog/dto/PostResponse.java
@@ -1,5 +1,6 @@
 package com.example.minglethedog.dto;
 
+import com.example.minglethedog.entity.Post;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 
@@ -10,7 +11,7 @@ public class PostResponse {
     private final String content;
     private final Long authorId;
 
-    public PostResponse(Long postId, String content, Long authorId) {
+    private PostResponse(Long postId, String content, Long authorId) {
         this.postId = postId;
         this.content = content;
         this.authorId = authorId;
@@ -20,6 +21,10 @@ public class PostResponse {
         this.postId = postId;
         this.content = null;
         this.authorId = null;
+    }
+
+    public static PostResponse of(Post post) {
+        return new PostResponse(post.getId(), post.getContent(), post.getAuthor().getId());
     }
 
 }
