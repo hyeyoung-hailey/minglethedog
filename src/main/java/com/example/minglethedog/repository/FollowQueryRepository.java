d693e09b5695e3f0b8d7c065680c960a5257c404
hyeyoung-hailey <hyeyounghaileysim@gmail.com>
2025-02-28 21:19:45 +0900

    feat: N+1 문제 방지를 위한 findFolloweeIdsByFollowingId 쿼리 최적화 - QueryDSL을 사용하여 findFolloweeIdsByFollowingId() 메서드 추가 - 한 번의 쿼리로 followee ID 목록을 조회하여 N+1 문제 해결

--- src/main/java/com/example/minglethedog/repository/FollowQueryRepository.java
+++ src/main/java/com/example/minglethedog/repository/FollowQueryRepository.java
@@ -1,2 +1,25 @@
-package com.example.minglethedog.repository;public class FollowQueryRepository {
+package com.example.minglethedog.repository;
+
+import com.example.minglethedog.entity.QFollow;
+import com.example.minglethedog.entity.User;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Repository
+public class FollowQueryRepository {
+    private final JPAQueryFactory queryFactory;
+
+    public List<Long> findFolloweeIdsByFollowingId(User user) {
+        QFollow follow = QFollow.follow;
+        return queryFactory
+                .select(follow.followee.id)  // followee의 ID만 선택
+                .from(follow)
+                .where(follow.follower.eq(user)) // follower가 해당 user인 경우
+                .fetch();
+    }
+
 }
