d49907dd171ef05ddadd66669cef695c1442d410
hyeyoung-hailey <hyeyounghaileysim@gmail.com>
2025-02-28 21:39:00 +0900

    perf: cdc구현으로 주석처리

--- src/main/java/com/example/minglethedog/messaging/MessageQueueTrigger.java
+++ src/main/java/com/example/minglethedog/messaging/MessageQueueTrigger.java
@@ -1,45 +1,38 @@
-package com.example.minglethedog.messaging;
-
-import com.example.minglethedog.dto.PostDto;
-import com.example.minglethedog.repository.PostQueryRepository;
-import com.example.minglethedog.repository.PostRepository;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
-import org.springframework.kafka.support.serializer.JsonSerializer;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-
-@Slf4j
-@Component
-@RequiredArgsConstructor
-public class MessageQueueTrigger {
-    private final RedisTemplate<String, String> redisTemplate;
-    private final PostRepository postRepository;
-    private final PostQueryRepository postQueryRepository;
-    private final KafkaTemplate<String, PostDto> kafkaTemplate;
-    private static final String MESSAGE_QUEUE = "post-queue";
-    private static final String TOPIC = "post-feed";
-
-    @Scheduled(fixedDelay = 5000) // 5초마다
-    public void processQueue() {
-        System.out.println("processQueue");
-        log.info("Executing processQueue...");
-        log.info("has key ::::" + redisTemplate.hasKey(MESSAGE_QUEUE));
-        while (Boolean.TRUE.equals(redisTemplate.hasKey(MESSAGE_QUEUE))) {
-            log.info("Queue exists, processing...");
-
-            String postId = redisTemplate.opsForList().rightPop(MESSAGE_QUEUE);
-            log.info("Post id: {}", postId);
-            if (postId != null) {
-                postQueryRepository.findByIdWithAuthorId(Long.parseLong(postId)).ifPresent(post -> {
-                    PostDto postDto = PostDto.of(post);
-                    kafkaTemplate.send(TOPIC, postDto);
-                    log.info("Post sent to Kafka : {}", postDto);
-                });
-            }
-        }
-    }
-}
+//package com.example.minglethedog.messaging;
+//
+//import com.example.minglethedog.dto.PostDto;
+//import com.example.minglethedog.repository.PostQueryRepository;
+//import com.example.minglethedog.repository.PostRepository;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.data.redis.core.RedisTemplate;
+//import org.springframework.kafka.core.KafkaTemplate;
+//import org.springframework.scheduling.annotation.Scheduled;
+//import org.springframework.stereotype.Component;
+//
+//@Slf4j
+//@Component
+//@RequiredArgsConstructor
+//public class MessageQueueTrigger {
+//    private final RedisTemplate<String, String> redisTemplate;
+//    private final PostRepository postRepository;
+//    private final PostQueryRepository postQueryRepository;
+//    private final KafkaTemplate<String, PostDto> kafkaTemplate;
+//    private static final String MESSAGE_QUEUE = "post-queue";
+//    private static final String TOPIC = "post-feed";
+//
+//    @Scheduled(fixedDelay = 5000) // 5초마다
+//    public void processQueue() {
+//        while (Boolean.TRUE.equals(redisTemplate.hasKey(MESSAGE_QUEUE))) {
+//            String postId = redisTemplate.opsForList().rightPop(MESSAGE_QUEUE);
+//            log.info("Post id: {}", postId);
+//            if (postId != null) {
+//                postQueryRepository.findByIdWithAuthorId(Long.parseLong(postId)).ifPresent(post -> {
+//                    PostDto postDto = PostDto.of(post);
+//                    kafkaTemplate.send(TOPIC, postDto);
+//                    log.info("Post sent to Kafka : {}", postDto);
+//                });
+//            }
+//        }
+//    }
+//}
